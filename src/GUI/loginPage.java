d17b2aa33d8cb9190329785fc191b09072c343d2
iremoykusezen <45572678+iremoykusezen@users.noreply.github.com>
2019-12-16 16:59:26 +0300

    updatedlogin

--- src/GUI/loginPage.java
+++ src/GUI/loginPage.java
@@ -1,11 +1,52 @@
 package GUI;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class loginPage {
 
     private String usertype;
+
+    private JButton loginButton;
+    private JTextField mailEntry;
+    private JTextField passwordEntry;
+
     public loginPage(String type){
 
         usertype=type;
-        OrganizerDashboard dash = new OrganizerDashboard();
+
+        JFrame frame = new JFrame("LOGIN PAGE");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1920,1080);
+
+        ImagePanel panel = new ImagePanel(new ImageIcon("C:\\Users\\iremoykusezen\\Documents\\GitHub\\CS_320_Bilet\\Background.jpg").getImage());
+        frame.getContentPane().add(panel);
+
+        JLabel email = new JLabel("E-mail:");
+        email.setFont(new Font("Calibri", Font.BOLD, 22));
+        email.setBounds(650, 250, 100, 22);
+        panel.add(email);
+
+        mailEntry = new JTextField("");
+        mailEntry.setFont(new Font("Courier", Font.PLAIN, 22));
+        mailEntry.setBounds(650, 300, 300, 30);
+        panel.add(mailEntry);
+
+
+        JLabel password = new JLabel("Password:");
+        password.setFont(new Font("Calibri", Font.BOLD, 22));
+        password.setBounds(650, 350, 250, 30);
+        panel.add(password);
+
+        passwordEntry = new JPasswordField("");
+        passwordEntry.setBounds(650, 400, 300, 30);
+        panel.add(passwordEntry);
+
+
+        loginButton = new JButton("Login");
+        loginButton.setBounds(750,450, 100,50);
+        panel.add(loginButton);
+
+        frame.setVisible(true);
     }
 }
