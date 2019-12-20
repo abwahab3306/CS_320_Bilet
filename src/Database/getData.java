3497b4461f6688b4202e46d65c3e119401239513
Abdul Wahab <30478933+abwahab3306@users.noreply.github.com>
2019-12-20 14:10:27 +0300

    Updated

--- src/Database/getData.java
+++ src/Database/getData.java
@@ -99,128 +99,7 @@ public class getData {
     }
 
 
-<<<<<<< HEAD
-    public static int getNumberOfDataInATable(String tableName) throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        int totalDataCount = 0;
-        PreparedStatement ps = connection.conn.prepareStatement("SELECT * FROM " + tableName);
-        ResultSet rs = ps.executeQuery();
-        while (rs.next()) {
-            totalDataCount += 1;
-            // do some work
-        }
-        connection.close();
-        return totalDataCount;
-    }
 
-    public static Organizer getLastInsertedOrganizerInATable() throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        PreparedStatement ps = connection.conn.prepareStatement("SELECT * FROM organizers");
-        ResultSet rs = ps.executeQuery();
-
-        String name = null;
-        String surname = null;
-        String mail = null;
-        String password = null;
-        while (rs.next()) {
-            name = rs.getString("name");
-            surname = rs.getString("surname");
-            mail = rs.getString("email");
-            password = rs.getString("password");
-
-        }
-        return new Organizer(name, surname, mail, password);
-    }
-
-    public static User getLastInsertedUserInATable() throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        PreparedStatement ps = connection.conn.prepareStatement("SELECT * FROM users");
-        ResultSet rs = ps.executeQuery();
-
-        String name = null;
-        String surname = null;
-        String mail = null;
-        String password = null;
-        while (rs.next()) {
-            name = rs.getString("name");
-            surname = rs.getString("surname");
-            mail = rs.getString("email");
-            password = rs.getString("password");
-
-        }
-        return new User(name, surname, mail, password);
-    }
-
-    public static User getUser(String email) throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        PreparedStatement ps = connection.conn.prepareStatement("SELECT * FROM users where email='" + email + "'");
-        ResultSet rs = ps.executeQuery();
-
-        String name = null;
-        String surname = null;
-        String mail = null;
-        String password = null;
-        while (rs.next()) {
-            name = rs.getString("name");
-            surname = rs.getString("surname");
-            mail = rs.getString("email");
-            password = rs.getString("password");
-
-        }
-        connection.close();
-        return new User(name, surname, mail, password);
-
-    }
-
-    public static Organizer getOrganizer(String email) throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        PreparedStatement ps = connection.conn.prepareStatement("SELECT * FROM organizers where email='" + email + "'");
-        ResultSet rs = ps.executeQuery();
-        String name = null;
-        String surname = null;
-        String mail = null;
-        String password = null;
-        while (rs.next()) {
-            name = rs.getString("name");
-            surname = rs.getString("surname");
-            mail = rs.getString("email");
-            password = rs.getString("password");
-
-        }
-        connection.close();
-        return new Organizer(name, surname, mail, password);
-
-    }
-
-
-    public static boolean loginUser(String name, String password) throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        String sql = "SELECT * from users WHERE email=? and  password=?";
-        PreparedStatement ps = connection.conn.prepareStatement(sql);
-        ps.setString(1, name);
-        ps.setString(2, password);
-        ResultSet rs = ps.executeQuery();
-        boolean isUserExist = false;
-        while (rs.next()) {
-            isUserExist = true;
-        }
-        return isUserExist;
-    }
-
-    public static boolean loginOrganizer(String name, String password) throws SQLException {
-        DB_Connection connection = new DB_Connection();
-        String sql = "SELECT * from organizers WHERE email=? and  password=?";
-        PreparedStatement ps = connection.conn.prepareStatement(sql);
-        ps.setString(1, name);
-        ps.setString(2, password);
-        ResultSet rs = ps.executeQuery();
-        boolean isUserExist = false;
-        while (rs.next()) {
-            isUserExist = true;
-        }
-        return isUserExist;
-    }
-=======
 	public static int getNumberOfDataInATable(String tableName) throws SQLException {
 		DB_Connection connection = new DB_Connection();
 		int totalDataCount = 0;
@@ -345,7 +224,6 @@ public class getData {
 		}
 		return isUserExist;
 	}
->>>>>>> 104e95b3cf45365678ef1ddad2e06dc7a91bba23
 
 
 }
